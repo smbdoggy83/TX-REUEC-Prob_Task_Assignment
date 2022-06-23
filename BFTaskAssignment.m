866cfac6e2a9c6a5b471050e6778570ccd60f063
Michael Paulson <73399845+mpaulson009@users.noreply.github.com>
2022-06-23 12:54:12 -0500

    Updated logic for finding min
    Account for possible duplicate inputs in the workList

--- BFTaskAssignment.m
+++ BFTaskAssignment.m
@@ -1,40 +1,45 @@
 % Brute Force Simulation Task Assignment on Edge Computing, this simulation
 % will serve as a baseline to compare FMS to.
 %% 
-
-% Parallel Arrays containing information about each device
-
 % We will test number of devices from 10 -> 20 in increments of 2
-numberOfDevices = 10;
+numberOfDevices = 20;
+% Minimum work requirement defined by user
 workRequired = 500;
+% Variables to keep track of current and min delay calculations 
 delay = 0;
 minDelay = intmax;
+% Position of the minimum set within our array
 position = 0;
+% Identifying indicies within our worklist of choosen items 
 indexList = {};
 
+% Parallel Arrays containing information about each device
 % Randomly generated mean from 10 -> 100 for reach deivce(Uniform Distro)
 minMean = 10; maxMean = 100;
-meanList = randi([minMean, maxMean], numberOfDevices,1)
+meanList = randi([minMean, maxMean], numberOfDevices,1);
 
 % Randomly generated std -5 -> 5 for reach deivce(Uniform Distro)
 minStd = -5; maxStd = 5;
-stdList = randi([minStd,maxStd], numberOfDevices,1)
+stdList = randi([minStd,maxStd], numberOfDevices,1);
 
 % Randomly generated work 100 -> 200 for each device (Uniform Distro)
 minWork = 100; maxWork = 200;
-workList = randi([minWork, maxWork ],numberOfDevices,1)
+workList = randi([minWork, maxWork ],numberOfDevices,1);
 
 % Randomly generated delay from above generated variables (Normal Distro)
-delayList = randn(numberOfDevices, 1)
+delayList = randn(numberOfDevices, 1);
+
+% Use individual Stds and Means to shift values to correct distro
 for i = 1:numberOfDevices
     delayList(i) = stdList(i) * delayList(i) + meanList(i);
 end
-disp(delayList)
+% Displaying our list of normally distrubuted values
+%disp(delayList)
 
-%find all combinations
+%find all combinations that meet work requirments
 combos = FindAll(workRequired, workList);
 
-%find best delay
+%find best delay from all combinations
 for i = 1:numel(combos)
     for j = 1:numel(combos{i})
         holder = find(workList == combos{i}(j));
@@ -42,19 +47,24 @@ for i = 1:numel(combos)
     end
     if delay<minDelay
         minDelay = delay;
-        position = i
+        position = i;
     end
     delay = 0;
 end
-display(combos{position})
-output = [minDelay];
-disp(output)
+disp(combos{position})
+output = minDelay;
+disp(min(output)) % Posiblility of duplicates, take the lower value 
+
 
 %%
-% Trying every combination
-% that meets the 500 work requirment, saving all of the combination's
-% indexes in a list and adding up their assocated delay in "delayList" to
-% find the mininum delay that completes the job.
+% Trying every combination that meets the 500 work requirment and adding 
+% up their assocated delay in "delayList" to find the mininum delay that 
+% completes the job.
+
+% Passed variables: workRequired (minimum work to be done)
+%                   workList (Work each device can complete before dead)
+% Returns: Complete list of all possible combinations which meet
+% workRequired
 
 function Combinations = FindAll(workRequired, workList)
 Combinations = {};
