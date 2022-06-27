1773bd4169b1dcce16938a27287405b351039e77
Michael Paulson <73399845+mpaulson009@users.noreply.github.com>
2022-06-27 14:28:32 -0500

    Using the binary approach

--- BFTaskAssignment.m
+++ BFTaskAssignment.m
@@ -2,9 +2,9 @@
 % will serve as a baseline to compare FMS to.
 %% 
 tic
-for loop = 10:2:20
+% for loop = 10:2:20
 % We will test number of devices from 10 -> 20 in increments of 2
-numberOfDevices = loop;
+numberOfDevices = 10;
 % Minimum work requirement defined by user
 workRequired = 500;
 % Variables to keep track of current and min delay calculations 
@@ -29,47 +29,59 @@ minWork = 100; maxWork = 200;
 workList = randi([minWork, maxWork ],numberOfDevices,1);
 
 % Randomly generated delay from above generated variables (Normal Distro)
-delayList = randn(numberOfDevices, 1);
+% delayList = randn(numberOfDevices, 1);
 
-% Use individual Stds and Means to shift values to correct distro
-for i = 1:numberOfDevices
-    delayList(i) = stdList(i) * delayList(i) + meanList(i);
-end
-% Displaying our list of normally distrubuted values
-%disp(delayList)
+% % Use individual Stds and Means to shift values to correct distro
+% for i = 1:numberOfDevices
+%     delayList(i) = stdList(i) * delayList(i) + meanList(i);
+% end
+% % Displaying our list of normally distrubuted values
+% %disp(delayList)
+ p = 0.95; % Probability constant, just arbitrarily setting it to this for now
+ A = sqrt(2) * erfinv(2*p - 1); % Constant
 
+AllX = {};
 %find all combinations that meet work requirments
-combos = FindAll(workRequired, workList);
-
-%find best delay from all combinations
-for i = 1:numel(combos)
-    for j = 1:numel(combos{i})
-        holder = find(workList == combos{i}(j));
-        delay = delay + min(delayList(holder(1:end)));
+for i = 1:(2^numberOfDevices)
+    check = dec2bin(i, numberOfDevices)
+    for j:numberOfDevices
+        if check(j) == "1"
+         % not sure if I should sum up every device individually in that
+         % equation or the total mu and std and then plug into the equation
     end
-    if delay(1:end)< min(minDelay)
-        minDelay = delay;
-        position = i;
-    end
-    delay = 0;
 end
 
-for i = 1:length(combos{position})
-indexList(find(workList == combos{position}(i))) = 1;
-end
+% combos = FindAll(workRequired, workList);
 
-toc
-
-items = rand(numberOfDevices, 1);
-for i = 1:length(items)
-    items(i) = i;
-end
-disp("-------------" + loop + " Devices----------------------------")
-T = table(items, indexList, delayList, workList)
-output = minDelay;
-disp("Min Delay: " + min(output)) % Posiblility of duplicates, take the lower value 
-disp("---------------------------------------------------")
-end
+% %find best delay from all combinations
+% for i = 1:numel(combos)
+%     for j = 1:numel(combos{i})
+%         holder = find(workList == combos{i}(j));
+%         delay = delay + min(delayList(holder(1:end)));
+%     end
+%     if delay(1:end)< min(minDelay)
+%         minDelay = delay;
+%         position = i;
+%     end
+%     delay = 0;
+% end
+% 
+% for i = 1:length(combos{position})
+% indexList(find(workList == combos{position}(i))) = 1;
+% end
+% 
+% toc
+% 
+% items = rand(numberOfDevices, 1);
+% for i = 1:length(items)
+%     items(i) = i;
+% end
+% disp("-------------" + loop + " Devices----------------------------")
+% T = table(items, indexList, delayList, workList)
+% output = minDelay;
+% disp("Min Delay: " + min(output)) % Posiblility of duplicates, take the lower value 
+% disp("---------------------------------------------------")
+% end
 
 
 %%
@@ -81,20 +93,27 @@ end
 %                   workList (Work each device can complete before dead)
 % Returns: Complete list of all possible combinations which meet
 % workRequired
+% 
+% function Combinations = FindAll(workRequired, workList)
+%     Combinations = {};
+%     for i = 1:numel(workList)
+%         findCombo(workList(i), workList(i+1:length(workList)))
+%     end
+% 
+%     function findCombo(current, rest)
+%         if sum(current) < workRequired
+%             for j = 1:numel(rest)
+%                 findCombo([current,rest(j)], rest(j+1:end))
+%             end
+%         else
+%             Combinations{end+1} = current;
+%         end
+%     end
+% end
+% AllX = dec2bin(0:2^n-1)' - '0'; all possible combinations function to try
+% export randomly generated data to use in other Algos
+% p = 0.99;
+% A = sqrt(2)*erfinv(2*p-1);
+% P3 has a built in function to solve
+% [x, fval] = intlinprog(R, intcon, A, b, Aeq, beq, lb, ub);
 
-function Combinations = FindAll(workRequired, workList)
-    Combinations = {};
-    for i = 1:numel(workList)
-        findCombo(workList(i), workList(i+1:length(workList)))
-    end
-
-    function findCombo(current, rest)
-        if sum(current) < workRequired
-            for j = 1:numel(rest)
-                findCombo([current,rest(j)], rest(j+1:end))
-            end
-        else
-            Combinations{end+1} = current;
-        end
-    end
-end
