08f06199a53b5a8edcbc11f61e09cd16aaf2906d
Seth Brodzik <sethmb83@gmail.com>
2022-06-30 23:38:52 -0400

    Further Organization + Prep
    Added FMS and RSS files with comments on what should be done, some steps taken on implementation
    
    Added all files to .prj file
    
    Updated w to "works" (list of device work abilities) in P2 for clarification

--- P2.m
+++ P2.m
@@ -1,17 +1,18 @@
-function [minWorkReq, totalMean, A, totalStd] = P2(means, stdDevs, w, p, W, x)
+function [minWorkReq, totalMean, A, totalStd] = P2(means, stdDevs, works, p, W, x)
 
 %% Represents equation P2. This function is meant to just perform calculations 
 %% and return results, whereas the true "minimization" is done by the algorithm
 %% that calls the method. 
-% Requirements: w, x, means, and stdDevs should all be the same length.
+% Requirements: works, x, means, and stdDevs should all be the same length.
 % Example: [minWorkReq, totalMean, A, totalStd] = P2(500, [3 4 10 9 7], [1 1 0 1 0], [23 12 76 52 82], [6 7 15 6 10])
 % 
 % Inputs: 
-% ~ W: For condition 1, total amount of work needed to meet or exceed. Ex: 500
-% ~ w (wi): Work accomplished per device. Ex: w[2] = work done by device 2
-% ~ x (xi): Chosen factor per device (1 = chosen, 2 = unchosen).  The paper refers to this as "vector X". Ex: x[2] = 1 --> device 2 was chosen.
 % ~ means (μi): The average delay per device. Ex: means[2] = average delay of device 2
 % ~ stdDevs (σi): The standard deviation of delay per device. Ex: stdDevs[2] = standard deviation of delay of device 2
+% ~ works (wi): Work accomplished per device. Ex: w[2] = work done by device 2
+% ~ p: Confidence probability. Usually 99% or 95%, represented as 0.99 or 0.95 .
+% ~ W: For condition 1, total amount of work needed to meet or exceed. Ex: 500
+% ~ x (xi): Chosen factor per device (1 = chosen, 2 = unchosen).  The paper refers to this as "vector X". Ex: x[2] = 1 --> device 2 was chosen.
 %
 % Outputs:
 % ~ minWorkReq: A boolean value representing if the minimum work
@@ -23,7 +24,7 @@ function [minWorkReq, totalMean, A, totalStd] = P2(means, stdDevs, w, p, W, x)
 
     % Finding the "such that" condition of P2
     n = length(x); % Number of devices
-    workReq = sum(w(1:n) .* x(1:n)); % The sum of all work per chosen device (sum from 1:n of w(i) * x(i) ). Must be >= W
+    workReq = sum(works(1:n) .* x(1:n)); % The sum of all work per chosen device (sum from 1:n of w(i) * x(i) ). Must be >= W
     minWorkReq = (workReq >= W);
 
     % Finding μ
